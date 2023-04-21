5fe6cf07e5918d68d951f084c47bbf3f1d406b30
xijia007 <jia.xi1@northeastern.edu>
2023-04-20 18:00:34 -0700

    add delete user from auth and users

--- src/routes/users.js
+++ src/routes/users.js
@@ -19,6 +19,8 @@ module.exports = (db, admin) => {
 
     router.delete('/:id', userController.deleteUser);
 
+    router.delete('/deleteAuthUser/:uid', userController.deleteAuthUser);
+
     router.post('/recordCurrentUser', userController.recordCurrentUser);
 
     router.post('/removeCurrentUser', userController.removeCurrentUser);
